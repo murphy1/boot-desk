b9c763b51659941edfaa2efb434628a440d175a7
Stephen Murphy <smurphy54321@gmail.com>
2019-10-01 13:35:55 -0400

    Initial commit

---
+++ src/main/java/com/murphy1/serviced/serviced/ServicedApplication.java
@@ -0,0 +1,13 @@
+package com.murphy1.serviced.serviced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicedApplication.class, args);
+    }
+
+}
