d6853255f380b70d8fd6ea95a88411777623449e
Stephen Murphy <smurphy54321@gmail.com>
2019-10-01 14:06:53 -0400

    Basic Homepage and controller has been coded. Closes #3

---
+++ src/main/java/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"/", "/index", "/home"})
+    public String homepage(){
+
+        return "index.html";
+    }
+
+}
