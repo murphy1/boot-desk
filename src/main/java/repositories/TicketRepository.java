f9070a21aae50835a159b689bf9870c1122e7632
Stephen Murphy <smurphy54321@gmail.com>
2019-10-01 14:10:11 -0400

    Repos have been created. Closes #2

---
+++ src/main/java/repositories/TicketRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import model.Ticket;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends CrudRepository<Ticket, Long> {
+}
---
+++ src/main/java/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
