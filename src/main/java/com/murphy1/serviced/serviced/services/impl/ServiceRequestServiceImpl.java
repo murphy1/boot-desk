ba4e6eef3f7a7e26af0498b0281cf810bd643f40
Stephen Murphy <smurphy54321@gmail.com>
2019-10-07 14:42:49 -0400

    Service Requests are now assigned to the correct user when created. Closes #21

--- src/main/java/com/murphy1/serviced/serviced/services/impl/ServiceRequestServiceImpl.java
+++ src/main/java/com/murphy1/serviced/serviced/services/impl/ServiceRequestServiceImpl.java
@@ -1,7 +1,13 @@
 package com.murphy1.serviced.serviced.services.impl;
 
-import com.murphy1.serviced.serviced.model.ServiceRequest;
-import com.murphy1.serviced.serviced.model.Status;
+import com.murphy1.serviced.serviced.model.*;
+import com.murphy1.serviced.serviced.repositories.AdminRepository;
+import com.murphy1.serviced.serviced.repositories.AgentRepository;
+import com.murphy1.serviced.serviced.repositories.EndUserRepository;
+import com.murphy1.serviced.serviced.services.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import com.murphy1.serviced.serviced.repositories.ServiceRequestRepository;
 import com.murphy1.serviced.serviced.services.ServiceRequestService;
@@ -14,11 +20,19 @@ import java.util.Optional;
 public class ServiceRequestServiceImpl implements ServiceRequestService {
 
     private ServiceRequestRepository serviceRequestRepository;
+    private AdminRepository adminRepository;
+    private AgentRepository agentRepository;
+    private EndUserRepository endUserRepository;
 
-    public ServiceRequestServiceImpl(ServiceRequestRepository serviceRequestRepository) {
+    private UserService userService;
+
+    public ServiceRequestServiceImpl(ServiceRequestRepository serviceRequestRepository, AdminRepository adminRepository, AgentRepository agentRepository, EndUserRepository endUserRepository, UserService userService) {
         this.serviceRequestRepository = serviceRequestRepository;
+        this.adminRepository = adminRepository;
+        this.agentRepository = agentRepository;
+        this.endUserRepository = endUserRepository;
+        this.userService = userService;
     }
-
 
     @Override
     public List<ServiceRequest> getAllServiceRequests() {
@@ -35,6 +49,31 @@ public class ServiceRequestServiceImpl implements ServiceRequestService {
 
         if (serviceRequest.getId() == null){
             serviceRequest.setStatus(Status.NEW);
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication.getPrincipal();
+
+            String username = ((UserDetails) principal).getUsername();
+
+            String role = userService.getRole(username);
+
+            List<ServiceRequest> serviceRequests;
+
+            if (role.equals("ADMIN")){
+                Optional<Admin> adminOptional = adminRepository.findByUsername(username);
+                serviceRequests = adminOptional.get().getServiceRequest();
+                serviceRequests.add(serviceRequest);
+            }
+            else if (role.equals("AGENT")){
+                Optional<Agent> agentOptional = agentRepository.findByUsername(username);
+                serviceRequests = agentOptional.get().getServiceRequest();
+                serviceRequests.add(serviceRequest);
+            }
+            else if (role.equals("END_USER")){
+                Optional<EndUser> endUserOptional = endUserRepository.findByUsername(username);
+                serviceRequests = endUserOptional.get().getServiceRequest();
+                serviceRequests.add(serviceRequest);
+            }
         }
 
         return serviceRequestRepository.save(serviceRequest);
